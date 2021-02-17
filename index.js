ed8fbc95e3f63b9c0f0717ad91cc5623430e72ea
glenn <glennwolfe@protonmail.com>
2021-02-17 03:05:44 +0000

    working

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+require('dotenv').config()
+const envalid = require('envalid')
+const { str, port, cleanEnv } = envalid
+const env = cleanEnv(process.env, {
+  LISTEN_PORT: port()
+})
 const express = require('express')
-const app = express()
 const md5 = require('md5')
-app.use(express.json())
 const { exec } = require("child_process");
+const fs = require('fs')
+const app = express()
 app.use(express.static('users'))
-
+app.use(express.json())
 app.post('/exec', async (req, res) => {
   if (req.body.method) {
     if (req.body.user_email) {
       let userHash = md5(req.body.user_email)
       if (req.body.method === 'create') {
-        // do something on create
-        const c = exec(`sudo ./wg.sh -a ${userHash}`)
-        console.log('created user', userHash)
-        res.status(200).send('ok:)')
+        if (!fs.existsSync(`./users/${userHash}`)) {
+          // do something on create
+          const c = exec(`sudo ./wg.sh -a ${userHash}`)
+          console.log('created user', userHash)
+          res.status(200).send('ok:)')
+        } else {
+          console.log('user already exist for', req.body.user_email)
+        }
       } else if (req.body.method === 'delete'){
         //do something on delete
         const c = exec(`sudo ./wg.sh -d ${userHash}`)
@@ -29,8 +39,7 @@ app.post('/exec', async (req, res) => {
     res.status(666).send({error: 'no method provided'})
   }
 });
-
-app.listen(5000)
+app.listen(env.LISTEN_PORT, ()=> console.log('app running on port', env.LISTEN_PORT))
 
 
 
